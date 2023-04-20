92b6fbe338af3200fa7cefd286bb074f5b341d20
ElNounch <ElNounch@users.noreply.github.com>
2023-04-20 06:34:23 -0400

    Update msys2.cmd

--- msys2.cmd
+++ msys2.cmd
@@ -1,4 +1,11 @@
 @echo off
-Call %MSYS2_CMD% %*
-Sleep 5
+set ARGS="%~1"
+Shift
+:Loop
+If "%~1" == "" GoTo Done
+Set ARGS=%ARGS%,'%~1'
+Shift
+GoTo Loop
+:Done
+powershell -Command "& {Exit (Start-Process -NoNewWindow -Wait -FilePath $env:MSYS2_CMD -ArgumentList $( %ARGS% ) -PassThru).ExitCode}"
 Exit %ErrorLevel%
