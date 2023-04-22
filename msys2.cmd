00ff778f406e5ab2b2a8a1d04372c7aa37b3f990
ElNounch <ElNounch@users.noreply.github.com>
2023-04-22 03:04:38 -0400

    Update msys2.cmd

--- msys2.cmd
+++ msys2.cmd
@@ -6,8 +6,5 @@ Set ARGS=%ARGS%,'%~1'
 Shift
 GoTo Loop
 :Done
-Rem PushD %MSYS2_ROOT%
 powershell -Command "{Exit (Start-Process -NoNewWindow -Wait -FilePath $env:MSYS2_CMD -ArgumentList @( %ARGS% ) -WorkingDirectory $pwd.Path  -PassThru).ExitCode}"
-Set Err=%ErrorLevel%
-Rem PopD
-Exit %Err%
+Exit %ErrorLevel%
