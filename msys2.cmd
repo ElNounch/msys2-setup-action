450cdc2323cf14c54cbf11b24d1e4ebba8a0d965
ElNounch <ElNounch@users.noreply.github.com>
2023-04-28 01:33:23 +0200

    Update msys2.cmd

--- msys2.cmd
+++ msys2.cmd
@@ -1,11 +1,2 @@
-Rem @echo off
-setlocal enabledelayedexpansion
-set ARGS='-here','-defterm','-shell','bash','-no-start'
-:Loop
-If "%~1" == "" GoTo Done
-Set ARGS=%ARGS%,'%~1'
-Shift
-GoTo Loop
-:Done
-powershell -Command "{Exit (Start-Process -NoNewWindow -Wait -FilePath $env:MSYS2_CMD -ArgumentList @( %ARGS% ) -WorkingDirectory $pwd.Path  -PassThru).ExitCode}"
-Exit %ErrorLevel%
+@start /WAIT /B %MSYS2_CMD% -here -defterm -shell bash -no-start %*
+@exit %ErrorLevel%
